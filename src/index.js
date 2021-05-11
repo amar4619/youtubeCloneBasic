649071eeb84a4b993bb348f80b02cd7766f6df08
Amarnath S <55694299+amarbhai@users.noreply.github.com>
2021-05-11 17:14:04 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list'
 import VideoDetail from './components/video_detail'
-const API_KEY = 'AIzaSyBoxy0nJ2SCh91zD419Ffam-Vc2-D_M9Y0';
+const API_KEY = 'xxxxxxxxxxxxKeyTobeInserted';
 
 
 
